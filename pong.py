0ea984e0fb916d9cb05bb7b6732075e081c60aee
hochunglai <92888439+hochunglai@users.noreply.github.com>
2021-10-24 11:05:28 -0400

    Major Update

--- pong.py
+++ pong.py
@@ -1,13 +1,76 @@
 import turtle
 
-def main():
+#Variable
+sta=False;
+
+#Screen Setup
+sc = turtle.Screen()
+sc.setup(600,800)
+sc.bgcolor("black")
+sc.title("Classic Pong")
     
-    sc = turtle.Screen()
+#Paddle Setup
+paddle = turtle.Turtle()
+turtle.setworldcoordinates(-100, -25, 100, 100)
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=1, stretch_len=4)
+paddle.penup()
 
-    sc.setup(600,800)
+#Ball setup
+ball = turtle.Turtle()
+ball.speed(0)
+ball.hideturtle()
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 10)
+ball.showturtle()
 
-    sc.bgcolor("black")
+def paddleright():
+    if sta==True:
+        x = paddle.xcor()
+        x += 10
+        paddle.setx(x)
 
-    sc.title("Classic Pong")
+def paddleleft():
+    if sta==True:
+        x = paddle.xcor()
+        x -= 10
+        paddle.setx(x)
+
+def startg():
+    global sta;
+    sta=True;
+
+#Keyboard bindings
+sc.listen()  
+sc.onkeypress(paddleright, "d")
+sc.onkeypress(paddleleft, "a")
+sc.onkeypress(startg, "space")
+
+
+def start():
+    starts = turtle.Turtle()
+    starts.speed(0)
+    starts.color("white")
+    starts.penup()
+    starts.hideturtle()
+    starts.goto(0, 80)
+    while sta==False:
+        starts.write("Press SpaceBar To Start",align="center", font=("Courier", 12, "normal"))
+    starts.clear()
+
+   
+
+def main():
+    start()
+  
+    while sta==True:
+        sc.update()
+
+    turtle.done()
     
 main();
+
+
