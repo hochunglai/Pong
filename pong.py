29c2216d5820e861ba0536153edab009cbd68253
hochunglai <92888439+hochunglai@users.noreply.github.com>
2021-10-24 14:40:28 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,7 +1,9 @@
 import turtle
+import random
 
 #Variable
 sta=False;
+score=0;
 
 #Screen Setup
 sc = turtle.Screen()
@@ -17,6 +19,7 @@ paddle.color("white")
 paddle.shapesize(stretch_wid=1, stretch_len=4)
 paddle.penup()
 
+
 #Ball setup
 ball = turtle.Turtle()
 ball.speed(0)
@@ -26,6 +29,15 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 10)
 ball.showturtle()
+
+#Start Setup
+starts = turtle.Turtle()
+starts.speed(0)
+starts.color("white")
+starts.penup()
+starts.hideturtle()
+starts.goto(0, 80)
+starts.write("Press SpaceBar To Start",align="center", font=("Courier", 12, "normal"))
 
 def paddleright():
     if sta==True:
@@ -40,8 +52,17 @@ def paddleleft():
         paddle.setx(x)
 
 def startg():
+    starts.clear()
+    starts.write("Score - {}".format(score), align="center",font=("Courier", 12, "normal"))
     global sta;
     sta=True;
+    main()
+
+def scoreup():
+    global score
+    score+=1
+    starts.clear()
+    starts.write("Score - {}".format(score), align="center",font=("Courier", 12, "normal"))
 
 #Keyboard bindings
 sc.listen()  
@@ -49,28 +70,50 @@ sc.onkeypress(paddleright, "d")
 sc.onkeypress(paddleleft, "a")
 sc.onkeypress(startg, "space")
 
-
-def start():
-    starts = turtle.Turtle()
-    starts.speed(0)
-    starts.color("white")
-    starts.penup()
-    starts.hideturtle()
-    starts.goto(0, 80)
-    while sta==False:
-        starts.write("Press SpaceBar To Start",align="center", font=("Courier", 12, "normal"))
-    starts.clear()
-
-   
-
 def main():
-    start()
-  
-    while sta==True:
-        sc.update()
+    #Ball Random x
+    sx=0;
+    while sx == 0:
+        sx = random.randrange(-2,5)
 
-    turtle.done()
+    #Ball Shoot y
+    sy=1;
+    while True:
+        sc.update()
+        print(sx)
+        #Set ball initial x shooting angle
+        x = ball.xcor()
+        x = x + sx;
+        ball.setx(x)
+
+        #Set ball initial y shooting angle
+        y = ball.ycor()
+        y = y + sy
+        ball.sety(y)
+
+        # Checking borders
+        if y > 98:
+            ball.sety(98)
+            sy = -1
+            scoreup()
+
+        if x < -100:
+            ball.setx(-100)
+            sx = sx*(-1)
+
+        if y < -25:
+            ball.sety(-25)
+            sy = 1
+
+        if x > 98:
+            ball.setx(98)
+            sx = sx*(-1)
+
+       #Collisions
+
+
+turtle.done()
     
-main();
+
 
 
